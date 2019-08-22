d5116b351e346080fe6d92d573e4ff69f95dc8f0
Svawek <svawek@gmail.com>
2019-08-22 16:51:08 +0300

    Add rspec to create(3/4)

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     it 'render index view' do
       get :index
@@ -11,8 +13,7 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #show' do
     it 'render show view' do
-      question = FactoryBot.create(:question)
-      get :show, params: { id: question }
+      get :show, params: { id: :question }
 
       expect(response).to render_template :show
     end
@@ -23,6 +24,30 @@ RSpec.describe QuestionsController, type: :controller do
       get :new
 
       expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question in the database' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
     end
   end
 end
