9d7fb0dcf7d6a9fde45f79504282ffb51227a085
Svawek <svawek@gmail.com>
2019-09-05 13:55:21 +0300

    Acceptance tests User can view all answers

---
+++ spec/features/answer/view_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User can view answers', %q{
+  In order to find right answer
+  I want to see all answer
+  On question page
+} do
+  given!(:question) { create(:question) }
+
+  scenario 'User can view all answers' do
+    visit questions_path
+    find('table tr:first-child a.view').click
+
+    fill_in 'Body', with: 'test_answer test_answer'
+    click_on 'Answer'
+
+    fill_in 'Body', with: 'test_answer2 test_answer2'
+    click_on 'Answer'
+
+    expect(page).to have_content 'test_answer test_answer'
+    expect(page).to have_content 'test_answer2 test_answer2'
+  end
+end
