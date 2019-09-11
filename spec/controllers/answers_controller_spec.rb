2b49bcdc00ddbce46da72d2b1e03e625f111ab22
svawek@ <svawek@gmail.com>
2019-09-11 22:59:12 +0300

    Fix(part3)

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:user) { create(:user) }
   let(:question) { create(:question, user_id: user.id) }
-  before { login(user) }
   
   describe 'GET #new' do
+    before { login(user) }
+
     it 'render new view' do
       get :new, params: { question_id: question }
 
@@ -14,51 +15,74 @@ RSpec.describe AnswersController, type: :controller do
   end
 
   describe 'POST #create' do
-    context 'with valid attributes' do
-      it 'saves a new answer in the database' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
-      end
+    context 'for authenticated use' do
+      before { login(user) }
 
-      it 'answer belong to question' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer) }
-        expect(assigns(:a_for_rspec).question.id).to eq question.id
+      context 'with valid attributes' do
+        it 'saves a new answer in the database' do
+          expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+        end
+  
+        it 'answer belong to question' do
+          post :create, params: { question_id: question, answer: attributes_for(:answer) }
+          expect(Answer.last.question).to eq question
+        end
+  
+        it 'redirects to show view' do
+          post :create, params: { question_id: question, answer: attributes_for(:answer) }
+          expect(response).to redirect_to question
+        end
       end
-
-      it 'redirects to show view' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer) }
-        expect(response).to redirect_to question
+  
+      context 'with invalid attributes' do
+        it 'does not save the answer' do
+          expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
+        end
+  
+        it 're-renders new view' do
+          post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+          expect(response).to render_template 'questions/show'
+        end
       end
     end
 
-    context 'with invalid attributes' do
-      it 'does not save the answer' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }.to_not change(Answer, :count)
-      end
+    context 'for nonauthenticated user' do
+      let(:question) { create(:question) }
 
-      it 're-renders new view' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
-        expect(response).to render_template 'questions/show'
+      it 'does not save the answer' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to_not change(Answer, :count)
       end
+    
     end
   end
 
   describe 'DELETE #destroy' do
     let!(:answer) { create(:answer, question: question, user: user) }
-    
-    it 'deletes the answer by author' do
-      expect { delete :destroy, params: { question_id: question, id: answer } }.to change(Answer, :count).by(-1)
+
+    context 'for authenticated user' do
+      let(:user2) { create(:user) }
+      
+      it 'deletes the answer by author' do
+        login(user)
+        expect { delete :destroy, params: { question_id: question, id: answer } }.to change(Answer, :count).by(-1)
+      end
+
+      it 'deletes the answer by nonauthor' do
+        login(user2)
+        expect { delete :destroy, params: { question_id: question, id: answer } }.to change(Answer, :count).by(0)
+      end
+
+      it 'redirect to index' do
+        login(user)
+        delete :destroy, params: { question_id: question, id: answer }
+        expect(response).to redirect_to questions_path
+      end
     end
 
-    it 'deletes the answer by nonauthor' do
-      sign_out(user)
-      user2 = FactoryBot.create(:user)
-      sign_in(user2)
-      expect { delete :destroy, params: { question_id: question, id: answer } }.to change(Answer, :count).by(0)
-    end
-
-    it 'redirect to index' do
-      delete :destroy, params: { question_id: question, id: answer }
-      expect(response).to redirect_to questions_path
+    context 'for nonautheticated user' do
+      it 'does not delete the answer' do
+        expect { delete :destroy, params: { question_id: question, id: answer } }.to change(Answer, :count).by(0)
+      end
     end
   end
 end
