5207e3a0dc044208558bc4ad03a6851aa909c60a
Svawek <svawek@gmail.com>
2019-09-24 23:13:01 +0300

    Author can choose the best answer(part1)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,16 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  def the_best
+    question = self.question
+    question.answers.each do |answer|
+      if answer.best
+        answer.best = false
+        return
+      end
+    end
+
+    self.best = true
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Answer, type: :model do
   it { should belong_to :user }
 
   it { should validate_presence_of :body }
+
+  context 'Best answer' do
+    let!(:user) { create(:user) }
+    let!(:question) { create(:question, user: user) }
+    let!(:answer) { create(:answer, question: question, user: user) }
+    let!(:answer2) { create(:answer, question: question, user: user) }
+
+    it 'question do not have  best answers' do
+      answer.the_best
+      expect(answer).to be_best(answer)
+    end
+
+    it 'question have best answer' do
+      answer.the_best
+      answer2.the_best
+      expect(answer).not_to be_best(answer)
+      expect(answer2).to be_best(answer2)
+    end
+  end
 end
